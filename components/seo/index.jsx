ba9423356bae0d9d3a69668154f84fd1bd71e476
dityath <dityaathallah107@gmail.com>
2021-11-29 16:06:44 +0700

    adding viewport on app js

--- components/seo/index.jsx
+++ components/seo/index.jsx
@@ -13,7 +13,6 @@ const Seo = () => {
         content="Contoh Undangan Pernikahan"
         key="og:description"
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </Head>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,20 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../scss/index.scss";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+import Head from "next/head";
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
 
 export default MyApp;
